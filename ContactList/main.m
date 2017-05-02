41562173f1860650efa672f184dcccefe6e0c8bd
Marc Maguire <maguire.marc@gmail.com>
2017-05-02 16:16:05 -0400

    initial commit

---
+++ ContactList/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Marc Maguire on 2017-05-02.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+
+//If you restart your app, all the contacts will be lost
+//Commit and push your progress on a regular basis
+//Seed some fake data in so you don’t have to create contacts each time you restart the app
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+    
+    
+    }
+    return 0;
+}
